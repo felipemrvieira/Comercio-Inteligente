4fd91e9c87fdb134ab6657e062ed11b2ebad26d0
unknown <felipemrvieira@gmail.com>
2016-06-19 12:25:14 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    include 'header.php';
+    include 'sidebar.php';
+    include 'content.php';
+    include 'page-footer.php';
+    include 'control-sidebar.php';
+    include 'footer.php';
+?>
+      
+
+      
+
+      
+
+  
+
+    
